4eae704831050ea89855684875dede474283d177
vishravi2016 <ravikumar.k1@tataelxsi.co.in>
2026-06-16 16:57:33 +0530

    another  change in test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ if(calc.add(10,20)!==30){
     throw new Error("addition test failed")
 }
 
-if(calc.multi(2,3)!==9){
+if(calc.multi(2,3)!==6){
     throw new Error("multiplication test failed")
 }
 
